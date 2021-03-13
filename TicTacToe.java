a5ecfb2313f816c4eaf31e82b375275c1d0bb0db
Daniel <emel.daniel2k@gmail.com>
2021-03-13 09:53:51 +0100

    Ver: 1.1 - Add game status && Enhanced input reading and grid printing

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,29 +7,69 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter cells: ");
-        String input = scanner.nextLine();
+        char[] grid = scanner.nextLine().toCharArray();
 
-        char[] grid = new char[input.length()];
-
-        for(int i = 0; i < input.length(); i++){
-            grid[i] = input.charAt(i);
-        }
-
+        // Printing out the grid that based on the user input
         System.out.println("---------");
-        String line = "";
-        for (int i = 0; i < input.length(); i++){
-            String tmp = line;
-            if (i % 3 == 0){
-                line = tmp.concat("| " + grid[i] + " ");
-            } else if ((i + 1) % 3 == 0 ){
-                line = tmp.concat(" " + grid[i] + " |");
-                System.out.println(line);
-                line = "";
-            } else {
-                line = tmp.concat(String.valueOf(grid[i]));
+        for (int i = 0; i < grid.length; i += 3) {
+            System.out.println("| " + grid[i] + " " + grid[i + 1] + " " + grid[i + 2] + " |");
+        }
+        System.out.println("---------");
+
+        // Logic and count variables init
+        boolean xWin = false, oWin = false;
+        int xCount = 0, oCount = 0;
+
+        // Counting the elements in the grid
+        for (char c : grid) {
+            if (c == 'X') {
+                xCount += 1;
+            } else if (c == 'O') {
+                oCount += 1;
             }
         }
-        System.out.println("---------");
-        // System.out.println("X O X\nO X O\nX X O");
+
+        int _Count = 9 - xCount - oCount;
+
+        // Horizontal and vertical check
+        for (int i = 0, j = 0; i < grid.length; i += 3, j += 1) {
+            if (grid[i] == grid[i + 1] && grid[i] == grid[i + 2]) {
+                if (grid[i] == 'X') {
+                    xWin = true;
+                } else if (grid[i] == 'O') {
+                    oWin = true;
+                }
+            }
+
+            if (grid[j] == grid[j + 3] && grid[j] == grid[j + 6]) {
+                if (grid[j] == 'X') {
+                    xWin = true;
+                } else if (grid[j] == 'O') {
+                    oWin = true;
+                }
+            }
+        }
+
+        // Checking diagonals
+        if ((grid[0] == grid[4] && grid[0] == grid[8]) || (grid[6] == grid[4] && grid[6] == grid[2])) {
+            if (grid[4] == 'X') {
+                xWin = true;
+            } else if (grid[4] == 'O') {
+                oWin = true;
+            }
+        }
+
+        // Printing out game state
+        if (Math.abs(xCount - oCount) > 1 || xWin && oWin) {
+            System.out.println("Impossible");
+        } else if (xWin) {
+            System.out.println("X wins");
+        } else if (oWin) {
+            System.out.println("O wins");
+        } else if (_Count == 0) {
+            System.out.println("Draw");
+        } else {
+            System.out.println("Game not finished");
+        }
     }
 }

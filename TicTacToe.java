6253637c06b41fb568288d307f39f8bc8b8562ee
Daniel <emel.daniel2k@gmail.com>
2021-03-17 23:57:02 +0100

    Add game mechanic for 2 players. Game status: Playable.

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,32 +4,78 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Reading game state from the user
-        System.out.println("Enter cells: ");
-        char[] grid = scanner.nextLine().toCharArray();
-
-        // Converting _ chars to ' ' (spaces).
-        for (int i = 0; i < grid.length; i++){
-            if (grid[i] == '_'){
-                grid[i] = ' ';
-            }
-        }
+        // Initialize the empty grid
+        char[] grid = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
         // Print out the grid before the step
         printGrid(grid);
 
+        // Step counter for choosing X or O
+        Integer step = 0;
+
         // Initialize Integer array for storing user input coordinates
         Integer[] coordinates = new Integer[2];
 
-        // Make a step
-        makeStep(grid, coordinates);
+        // Where the game happens
+        while(true){
+            makeStep(grid, coordinates, step);
+            analyzeGameStatus(grid);
+        }
+    }
 
+    // Printing out the grid that is based on the user input
+    public static void printGrid(char[] grid){
+        System.out.println("---------");
+        for (int i = 0; i < grid.length; i += 3) {
+            System.out.println("| " + grid[i] + " " + grid[i + 1] + " " + grid[i + 2] + " |");
+        }
+        System.out.println("---------");
+    }
 
-        // Logic and count variables init
+    // Make a step if the user coordinates are valid
+    public static void makeStep(char[] grid, Integer[] coordinates, Integer step) {
+        getCoordinates(grid, coordinates);
+        if (step % 2 == 0){
+            grid[(coordinates[0] - 1) * 3 + coordinates[1] - 1] = 'X';
+        } else {
+            grid[(coordinates[0] - 1) * 3 + coordinates[1] - 1] = 'Y';
+        }
+        printGrid(grid);
+    }
 
-        /* boolean xWin = false, oWin = false;
+    // Get coordinates from user until they are valid according to our terms
+    public static void getCoordinates(char[] grid, Integer[] coordinates){
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+
+        System.out.println("Enter the coordinates: ");
+        while (i < 2) {
+            if (scanner.hasNextInt()) {
+                int input = scanner.nextInt();
+                if (input < 1 || input > 3) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    getCoordinates(grid, coordinates);
+                    break;
+                } else {
+                    coordinates[i] = input;
+                    i++;
+                }
+            } else {
+                System.out.println("You should enter numbers!");
+                getCoordinates(grid, coordinates);
+                break;
+            }
+        }
+
+        if(grid[(coordinates[0] - 1) * 3 + coordinates[1] - 1] != ' '){
+            System.out.println("This cell is occupied! Choose another one!");
+            getCoordinates(grid, coordinates);
+        }
+    }
+
+    // Analyzing the game status
+    public static void analyzeGameStatus(char[] grid){
+        boolean xWin = false, oWin = false;
         int xCount = 0, oCount = 0;
 
         // Counting the elements in the grid
@@ -41,7 +87,7 @@ public class Main {
             }
         }
 
-        int _Count = 9 - xCount - oCount;
+        int emptyCell = 9 - xCount - oCount;
 
         // Horizontal and vertical check
         for (int i = 0, j = 0; i < grid.length; i += 3, j += 1) {
@@ -72,68 +118,15 @@ public class Main {
         }
 
         // Printing out game state
-        if (Math.abs(xCount - oCount) > 1 || xWin && oWin) {
-            System.out.println("Impossible");
-        } else if (xWin) {
+        if (xWin) {
             System.out.println("X wins");
+            System.exit(0);
         } else if (oWin) {
             System.out.println("O wins");
-        } else if (_Count == 0) {
+            System.exit(0);
+        } else if (emptyCell == 0) {
             System.out.println("Draw");
-        } else {
-            System.out.println("Game not finished");
-        } */
-    }
-
-    // Printing out the grid that based on the user input
-    public static void printGrid(char[] grid){
-        System.out.println("---------");
-        for (int i = 0; i < grid.length; i += 3) {
-            System.out.println("| " + grid[i] + " " + grid[i + 1] + " " + grid[i + 2] + " |");
-        }
-        System.out.println("---------");
-    }
-
-    // Make a step if the user coordinates are valid
-    public static void makeStep(char[] grid, Integer[] coordinates) {
-        getCoordinates(grid, coordinates);
-        grid[(coordinates[0] - 1) * 3 + coordinates[1] - 1] = 'X';
-        printGrid(grid);
-
-        /* for (char c: grid){
-           if (c == ' '){
-               makeStep(grid);
-          }
-        }*/
-    }
-
-    // Get coordinates from user until they are valid according to our terms
-    public static void getCoordinates(char[] grid, Integer[] coordinates){
-        Scanner scanner = new Scanner(System.in);
-        int i = 0;
-
-        System.out.println("Enter the coordinates: ");
-        while (i < 2) {
-            if (scanner.hasNextInt()) {
-                int input = scanner.nextInt();
-                if (input < 1 || input > 3) {
-                    System.out.println("Coordinates should be from 1 to 3!");
-                    getCoordinates(grid, coordinates);
-                    break;
-                } else {
-                    coordinates[i] = input;
-                    i++;
-                }
-            } else {
-                System.out.println("You should enter numbers!");
-                getCoordinates(grid, coordinates);
-                break;
-            }
-        }
-
-        if(grid[(coordinates[0] - 1) * 3 + coordinates[1] - 1] != ' '){
-            System.out.println("This cell is occupied! Choose another one!");
-            getCoordinates(grid, coordinates);
+            System.exit(0);
         }
     }
 }
